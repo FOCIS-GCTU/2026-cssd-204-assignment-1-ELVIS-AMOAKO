ef9d10f43c1fdb81f0f666d2a475def73e8a78ef
Elvis Amoako <2425400297@live.gctu.edu.gh>
2026-05-31 22:10:41 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,51 @@
+# File: initials.py
+
+# Description: Print out my initials.
+
+# Assignment Number: 1
+
+#
+
+# Name: Amoako Acheampong Elvis
+
+# STUDENT ID: 2425400297
+
+# Email: elvisamoako332@gmail.com
+
+# Grader: Mr.Augustus
+
+#
+
+# On my honor, Amoako Acheampong Elvis, this programming assignment is my own work
+
+# and I have not provided this code to any of the student.
 
 def main():
-  pass #code goes here
+     #print my initials
 
+     print()
+     print("...EAA")
+     print()
+     print("...EEEEEEEEEEEE..........AAA.AAA.............AAA.AAA........")
+     print("...EEEE.................AAA....AAA..........AAA....AAA......")
+     print("...EEEE................AAA......AAA........AAA......AAA.....")
+     print("...EEEEEEEEEEEE........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+     print("...EEEEEEEEEEEE........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+     print("...EEEE................AAA......AAA........AAA......AAA.....")
+     print("...EEEE................AAA......AAA........AAA......AAA.....")
+     print("...EEEEEEEEEEEE...**...AAA......AAA...**...AAA......AAA...**")
+     print("...EEEEEEEEEEEE...**...AAA......AAA...**...AAA......AAA...**")
 
+     print()
 
 main()
+#final submission
+
+
+
+
+
+
+
+
+
